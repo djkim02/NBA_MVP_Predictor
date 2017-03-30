df0e690943420d7696cbbcf775a776aa3e098788
Dong Joon Kim <djkim02@ucla.edu>
2017-03-30 16:51:33 -0700

    Added get_stats function

--- mvp_parser.py
+++ mvp_parser.py
@@ -3,6 +3,8 @@ import requests
 import cPickle as pickle
 from nba_py import player
 from nba_py.player import get_player
+from nba_py.player import PlayerGeneralSplits
+import json
 
 # Returns the top 40 scoring leaders of a season.
 # if year = 2000, this function returns the top 40 scoring leaders of 1999-00 season
@@ -43,16 +45,37 @@ def read_scoring_leaders_from_a_pickle(scoring_leaders_pkl_fn):
 	top40_scorers = pickle.load(open(scoring_leaders_pkl_fn, 'rb'))
 	return top40_scorers
 
-
+# Returns first and last name given a full name
 def parse_first_and_last_name(name):
+	first_and_last_name = player.split(' ', 1)
+	first_name = first_and_last_name[0]
+	last_name = first_and_last_name[1]
+	return first_name, last_name
+
+# Given a season (one integer) returns a formatted season
+# ex) if season = 2000, returns "1999-00"
+# 	  if season = 2017, returns "2016-17"
+def format_season(season):
+	return str(season-1) + "-" + str(season)[2:]
 
 
 # Returns the statistics of a player in a season
-def get_stats(name, season):
-	return
+def get_stats(first_name, last_name, season):
+	formatted_season = format_season(season)
+	player_id = get_player(first_name, last_name, season=formatted_season)
+	stats = PlayerGeneralSplits(player_id, season=formatted_season)
+	return json.dumps(stats.json)
+
 
 
 if __name__ == "__main__":
 	scoring_leaders_pkl_fn = 'scoring_leaders.pkl'
 	scoring_leaders = read_scoring_leaders_from_a_pickle(scoring_leaders_pkl_fn)
-	print scoring_leaders
+	seasons = scoring_leaders.keys()
+
+	for season in seasons:
+		for player in scoring_leaders[season]:
+			first_name, last_name = parse_first_and_last_name(player)
+			stats = get_stats(first_name, last_name, season)
+			break
+		break
